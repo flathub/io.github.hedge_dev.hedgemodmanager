ab859f490c05aef0f729c453de80091d68c6c259
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2025-03-08 22:58:36 +1100

    Update to 8.0.0-beta4

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-flatpak-builder --user --install --force-clean --install-deps-from=flathub --repo repo builddir io.github.hedge_dev.hedgemodmanager.yml
+export APP_ID=io.github.hedge_dev.hedgemodmanager
+flatpak-builder --user --install --force-clean --install-deps-from=flathub --repo repo builddir ${APP_ID}.yml
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions manifest ${APP_ID}.yml
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions appstream ${APP_ID}.metainfo.xml
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions repo repo
